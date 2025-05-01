e84ff5ad22a0af86c81d078c63cc5e2954b3d36a
aliasghar144 <110099930+aliasghar144@users.noreply.github.com>
2025-05-01 13:09:21 +0330

    Create login_use_case.dart

---
+++ __brick__/lib/features/{{name}}/domain/usecases/login_use_case.dart
@@ -0,0 +1,12 @@
+// class loginUseCase extends UseCase<Either<ErrorModel,LoginEntity>,void>{
+
+//   final {{name.pascalCase()}}Repository {{name.snakeCase()}}Repository;
+
+//   loginUseCase(this.{{name.snakeCase()}}Repository);
+
+//   @override
+//   Future<Either<ErrorModel, CheckAppEntity>> call(void param) {
+//     return {{name.snakeCase()}}Repository.login();
+//   }
+
+// }
