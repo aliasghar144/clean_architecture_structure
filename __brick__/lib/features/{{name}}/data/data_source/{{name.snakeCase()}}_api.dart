745d557aceabedbb17c15701e6ee04d406079960
aliasghar144 <110099930+aliasghar144@users.noreply.github.com>
2025-05-01 13:02:13 +0330

    Update {{name.snakeCase()}}_api.dart

--- __brick__/lib/features/{{name}}/data/data_source/{{name.snakeCase()}}_api.dart
+++ __brick__/lib/features/{{name}}/data/data_source/{{name.snakeCase()}}_api.dart
@@ -1,4 +1,11 @@
 class {{name.pascalCase()}}Api {
-const {{name.pascalCase()}}Api();
+
+  // Future<Response> login() async {
+  //   final result = await DioHelper.dio.get(
+  //     '/',
+  //   );
+  //   return result;
+  // }
+
 
 }
