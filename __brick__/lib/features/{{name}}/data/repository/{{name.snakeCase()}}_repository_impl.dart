98366e0464c4b3e3555dfed1b7e0ca6fc297bae0
aliasghar144 <110099930+aliasghar144@users.noreply.github.com>
2025-05-01 13:03:48 +0330

    Update {{name.snakeCase()}}_repository_impl.dart

--- __brick__/lib/features/{{name}}/data/repository/{{name.snakeCase()}}_repository_impl.dart
+++ __brick__/lib/features/{{name}}/data/repository/{{name.snakeCase()}}_repository_impl.dart
@@ -6,4 +6,14 @@ import '../data_source/{{name.pascalCase()}}_api.dart';
 class {{name.pascalCase()}}RepositoryImpl extends {{name.pascalCase()}}Repository {
     {{name.pascalCase()}}Api {{name.snakeCase()}}Api;
     {{name.pascalCase()}}RepositoryImpl(this.{{name.snakeCase()}}Api);
+
+
+  // @override
+  // Future<Either<ErrorModel, CheckAppEntity>> login() async {
+  //   return await safeApiCall(() async {
+  //     Response response = await splashApi.login();
+  //     return loginModel.fromJson(response.data);
+  //   },);
+  // }
+
 }
